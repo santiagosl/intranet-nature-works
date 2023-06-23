1b3e2113f9fdfeb5a449832fcd69f2228489cf28
Santiago <santiagosl314@hotmail.com>
2023-06-23 18:56:20 +0200

    agregar una clase a listado de pedidos

--- resources/views/pedidos/show.blade.php
+++ resources/views/pedidos/show.blade.php
@@ -126,12 +126,19 @@
                         </tbody>
                    </table>
 
+                @php
+                $contador = 0;
+                @endphp
+                   
                    <table class="table-auto w-full mt-5">
                     @foreach ($pdf as $item)
+                    @php
+                    $contador++;
+                    @endphp
                     <thead  class="text-xs font-semibold uppercase text-gray-400 bg-green-50">
                         <tr>
                             <th colspan="2" class="p-2">
-                                <div class="font-semibold text-left">Documentación</div>
+                                <div class="font-semibold text-left">Documento -> {{$contador}}</div>
                             </th>
 
                         </tr>
@@ -140,7 +147,7 @@
                         <tr>
                          
                         <td class="p-2">
-                            <a href="storage/{{$item->url}}" target="_blank">Ver documento</a>
+                            <a href="storage/{{$item->url}}" target="_blank">{{$item->url}}</a>
                         </td>
                            
                         </tr>
@@ -153,7 +160,7 @@
                 <div class="mt-20 w-full text-center inline-block">
                     {!! Form::model($pedido, ['route' => ['pedidos.update' , $pedido], 'method' => 'put']) !!}
                         {!! Form::hidden('status', 'Finalizado') !!}    
-                    {!! Form::submit('Finalizar pedido', ['class' => 'w-full text-center inline-block rounded bg-green-300 px-2 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]'] ) !!}
+                    {!! Form::submit('Finalizar pedido', ['class' => 'cursor-pointer w-full text-center inline-block rounded bg-green-300 px-2 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]'] ) !!}
                     {!! Form::close() !!}
                 </div>
             </div>
